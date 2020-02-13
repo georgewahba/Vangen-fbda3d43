f527bcc7ac28b931359c6aee57409e9df0549ea1
GeorgeWahba <george-wahba@hotmail.com>
2020-02-13 10:27:34 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,67 @@
+function gooiButton() {
+    try {
+        bal.gooi();
+    } catch (error) {
+        document.getElementById("error").innerHTML = "probeer opnieuw";
+    }
+}
+
+function vangButton() {
+    try {
+        bal.vang();
+    } catch (error) {
+        document.getElementById("error").innerHTML = "probeer opnieuw";
+    }
+
+}
+
+function resetButton() {
+    bal.reset();
+    document.getElementById("error").innerHTML = " "
+}
+
+
+var bal = {
+    canvasBal: constructBal(),
+    balPositie: "links",
+
+    gooi: function () {
+        if (this.balPositie !== "links") {
+            throw Error("bal in verkeerde positie")
+        }
+        this.draw(300, 50);
+        this.balPositie = "midden";
+    },
+
+    vang: function () {
+        if (this.balPositie !== "midden") {
+            throw Error("bal in verkeerde positie")
+        }
+        this.draw(500, 250);
+        this.balPositie = "rechts";
+    },
+
+    reset: function () {
+        this.draw(100, 250);
+        this.balPositie = "links";
+    },
+
+    draw: function (x, y) {
+        this.canvasBal.clearRect(0, 0, 600, 300);
+        this.canvasBal.beginPath();
+        this.canvasBal.arc(x, y, 50, 0, 2 * Math.PI);
+        this.canvasBal.closePath();
+        this.canvasBal.fill();
+    }
+}
+
+function constructBal() {
+    let bal = document.getElementById("bal").getContext("2d");
+    bal.fillStyle = "red";
+    bal.beginPath();
+    bal.arc(100, 250, 50, 0, 2 * Math.PI);
+    bal.closePath();
+    bal.fill();
+    return bal
+}
+
